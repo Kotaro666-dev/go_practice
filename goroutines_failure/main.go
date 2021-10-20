1f7a0ee68600afc67cb8ce11caea535de43277b4
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-21 08:28:48 +0900

    Update: Add pattern to make it work

--- goroutines_failure/main.go
+++ goroutines_failure/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func sayHello() {
 	fmt.Println("say hello")
@@ -11,4 +13,19 @@ func main() {
 	go sayHello()
 	fmt.Println("say hello from main goroutine")
 }
+
+// 以下は失敗パターンを成功させるパターン。goroutines時にwaitさせることで、goroutines処理が終わるをまってから、プログラムを終了する
+//func sayHello(wg *sync.WaitGroup) {
+//	defer wg.Done()
+//	fmt.Println("say hello")
+//}
+//
+//func main() {
+//	// go sayHello() によりゴルーチンが生成(fork)されるがメインゴルーチンの処理が終わるまでにその実行タイミングが無い
+//	var wg sync.WaitGroup
+//	wg.Add(1)
+//	go sayHello(&wg)
+//	wg.Wait()
+//	fmt.Println("say hello from main goroutine")
+//}
 
