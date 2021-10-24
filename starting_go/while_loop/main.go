9360c7e0d4ecd4c834f7d2dfb0044d33a44735ea
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-24 13:37:52 +0900

    Update: Add codes after go fmt

--- starting_go/while_loop/main.go
+++ starting_go/while_loop/main.go
@@ -3,13 +3,13 @@ package main
 import f "fmt"
 
 func main() {
-  i := 0;
+	i := 0
 
-  for {
-    f.Println(i);
-    if (i == 10) {
-      break;
-    }
-    i++;
-  }
+	for {
+		f.Println(i)
+		if i == 10 {
+			break
+		}
+		i++
+	}
 }
