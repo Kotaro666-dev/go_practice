8b409f1fe28e6f3be667325ff1efdcf74b2eab60
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-24 12:21:18 +0900

    Update: Add app.go

--- starting_go/zoo/main.go
+++ starting_go/zoo/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello world!");
+  fmt.Println("Hello world!")
+  fmt.Println(AppName());
   fmt.Println(animals.ElephantFeed());
   fmt.Println(animals.MonkeyFeed());
   fmt.Println(animals.RabbitFeed());
