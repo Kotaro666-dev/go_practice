1037925744207c67c74539d031ac8bdf72ffcf8d
Kotaro Kamashima <kotaro0726@gmail.com>
2021-12-12 01:35:21 +0900

    Update: Add feature to return xml

--- api_with_json/main.go
+++ api_with_json/main.go
@@ -3,10 +3,68 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Menu struct {
+	XMLName  xml.Name   `xml:"menu"`
+	Contents []Contents `xml:"contents"`
+}
+
+type Contents struct {
+	Index int    `xml:"index"`
+	Text  string `xml:"text"`
+	Url   string `xml:"url"`
+}
+
+var MenuData = Menu{
+	Contents: []Contents{
+		Contents{
+			Index: 0,
+			Text:  "Menu00",
+			Url:   "/test00",
+		},
+		Contents{
+			Index: 1,
+			Text:  "Menu01",
+			Url:   "/test01",
+		},
+		Contents{
+			Index: 2,
+			Text:  "Menu02",
+			Url:   "/test03",
+		},
+		Contents{
+			Index: 3,
+			Text:  "Menu03",
+			Url:   "/test03",
+		},
+		Contents{
+			Index: 4,
+			Text:  "Menu04",
+			Url:   "/test04",
+		},
+	},
+}
+
+func HandlerXml(w http.ResponseWriter, r *http.Request) {
+	var buffer bytes.Buffer
+	encoder := xml.NewEncoder(&buffer)
+	encoder.Indent("", "\t") // 各行にインデントを追加
+	if err := encoder.Encode(&MenuData); err != nil {
+		log.Fatal(err)
+	}
+	output := xml.Header + buffer.String()
+	_, err := fmt.Fprint(w, output)
+	if err != nil {
+		fmt.Println("Error encoding XML: ", err)
+		return
+	}
+	fmt.Println("Returning XML data successfully.")
+}
 
 type CustomerInfo struct {
 	Id            int    `json:"id"`
@@ -24,7 +82,7 @@ var CustomerInfoData = CustomerInfo{
 	DepositAmount: 1500000,
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func HandlerJson(w http.ResponseWriter, r *http.Request) {
 	var buffer bytes.Buffer
 	encoder := json.NewEncoder(&buffer)
 	if err := encoder.Encode(&CustomerInfoData); err != nil {
@@ -35,11 +93,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error encoding JSON: ", err)
 		return
 	}
-  fmt.Println("Returning JSON data successfully.")
+	fmt.Println("Returning JSON data successfully.")
 }
 
 func main() {
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/json", HandlerJson)
+	http.HandleFunc("/xml", HandlerXml)
 	log.Println("Start listening server...")
 	http.ListenAndServe(":8080", nil)
 }
